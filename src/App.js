42e542c466118fa833a21f59d1756e05b4d2ad93
leonardo <leoleitao27@gmail.com>
2022-11-12 21:03:36 -0300

    css update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <Barra/>
         
+        <Barra/>
+
       </header>
     </div>
   );
--- src/components/Barra.js
+++ src/components/Barra.js
@@ -1,12 +1,16 @@
 import styles from './Barra.module.css'
+import logomarca from '../img/logo.png'
 
 
 function barra(){
     return(
         <>
             <div className='barra-de-navegacao'>
+                
+                <img src={logomarca}/>
 
                 <a>Login</a>
+
                 <ul>
                     <li>Planos</li>
                     <li>Suporte</li>
